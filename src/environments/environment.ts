8df64a2b739be36a580cd22b8c8c33ba59437c96
Luis Stiven Arteaga Solano <stivenartesol@gmail.com>
2019-06-19 22:42:33 -0500

    se agrego configuracion proxy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  HOST_URL:'http://localhost:8080/servicio'
+  HOST_URL:'/servicio'
 };
-
+//  //HOST_URL:'http://localhost:8080/servicio'
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
